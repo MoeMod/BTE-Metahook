118067d299abf99dc48d9e04baced320c55e44c0
HowToDoThis <nopeople012345@gmail.com>
2018-03-11 19:51:59 +0800

    修复上次所不完整的文件+错误档案..新增WpnData【未完成】

--- GameUI/CSBTEUpdateDialog.cpp
+++ GameUI/CSBTEUpdateDialog.cpp
@@ -1,236 +1,33 @@
-#include "base.h"
-#include "hud.h"
-#include "configs.h"
 #include "CSBTEUpdateDialog.h"
-#include "EngineInterface.h"
-#include <vgui_controls\PHandle.h>
-
-#include "Client/HUD/hud_saytext.h"
-
-static void(*pfnUnknown)() = (void(*)())0x1960870;
-
 
 CCSBTEUpdateDialog::CCSBTEUpdateDialog(Panel *parent, const char *panelName, bool showTaskbarIcon) : Frame(parent, panelName, showTaskbarIcon)
 {
 	int sw, sh;
 	surface()->GetScreenSize(sw, sh);
 
-	SetTitle("", false);
+	SetTitle("#CSBTE_UpdateNews_Title", false);
 
-	SetBounds(sw / 2 - 200, sh / 2 - 150, 450, 200);
+	SetSize(740, 700);
+	MoveToCenterOfScreen();
 	SetSizeable(false);
 	SetVisible(true);
 
-
-	m_pLogo = new ImagePanel(this, "CSBTELogo");
-	m_pLogo->SetImage(scheme()->GetImage("resource/game_menu_mouseover2", false));
-
-	m_pLogo->SetBounds(130, 25, 300, 40);
-
-	m_pMoreInfoLabel = new Label(this, "MoreInfoLabel", "#CSBTE_About_MoreInformation");
-	m_pMoreInfoLabel->SetPaintBackgroundEnabled(false);
-	m_pMoreInfoLabel->SetBounds(10, 70, 200, 20);
-
-	m_pCsoldjbURL = new URLLabel(this, "CsoldjbTiebaURL", "#CSBTE_About_DjbBar", "http://tieba.baidu.com/csoldjb");
-	m_pCsoldjbURL->SetPaintBackgroundEnabled(false);
-	if (Q_stricmp(gConfigs.szLanguage, "english"))
-		m_pCsoldjbURL->SetBounds(180, 68, 150, 24);
-	else
-		m_pCsoldjbURL->SetBounds(200, 68, 150, 24);
-
-	wchar_t *wstrSwitch[2] = { localize()->Find("#CSBTE_SwitchOn"), localize()->Find("#CSBTE_SwitchOff") };
-
-	if (!wstrSwitch[0] || wstrSwitch[0][0] == L'#')
-		wstrSwitch[0] = L"On";
-	if (!wstrSwitch[1] || wstrSwitch[1][0] == L'#')
-		wstrSwitch[1] = L"Off";
-
-	m_pNewHUDSwitchLabel = new Label(this, "NewHUDSwitchLabel", "#CSBTE_NewHUDLabel");
-	m_pNewHUDSwitchLabel->SetPaintBackgroundEnabled(false);
-	m_pNewHUDSwitchLabel->SetBounds(10, 90, 100, 20);
-
-	m_pNewHUDSwitch = new ComboBox(this, "NewHUDSwitch", 2, false);
-	m_pNewHUDSwitch->AddItem(wstrSwitch[0], NULL);
-	m_pNewHUDSwitch->AddItem(wstrSwitch[1], NULL);
-	m_pNewHUDSwitch->ActivateItem(gConfigs.bEnableNewHud ? 0 : 1);
-	m_pNewHUDSwitch->SetBounds(10, 120, 100, 24);
-
-	m_pNewMuzzleflashSwitchLabel = new Label(this, "NewMuzzleFlashSwitchLabel", "#CSBTE_NewMuzzleFlashLabel");
-	m_pNewMuzzleflashSwitchLabel->SetPaintBackgroundEnabled(false);
-	m_pNewMuzzleflashSwitchLabel->SetBounds(120, 90, 100, 20);
-
-	m_pNewMuzzleflashSwitch = new ComboBox(this, "NewMuzzleFlashSwitch", 2, false);
-	m_pNewMuzzleflashSwitch->AddItem(wstrSwitch[0], NULL);
-	m_pNewMuzzleflashSwitch->AddItem(wstrSwitch[1], NULL);
-	m_pNewMuzzleflashSwitch->ActivateItem(gConfigs.bEnableNewMuzzle ? 0 : 1);
-	m_pNewMuzzleflashSwitch->SetBounds(120, 120, 100, 24);
-
-	m_pFireDlightSwitchLabel = new Label(this, "FireDlightSwitchLabel", "#CSBTE_FireDlightLabel");
-	m_pFireDlightSwitchLabel->SetPaintBackgroundEnabled(false);
-	m_pFireDlightSwitchLabel->SetBounds(230, 90, 100, 20);
-
-	m_pFireDlightSwitch = new ComboBox(this, "FireDlightSwitch", 2, false);
-	m_pFireDlightSwitch->AddItem(wstrSwitch[0], NULL);
-	m_pFireDlightSwitch->AddItem(wstrSwitch[1], NULL);
-	m_pFireDlightSwitch->ActivateItem(gConfigs.bEnableFireDlight ? 0 : 1);
-	m_pFireDlightSwitch->SetBounds(230, 120, 100, 24);
-
-	m_pBloodSwitchLabel = new Label(this, "BloodSwitchLabel", "#CSBTE_BloodLabel");
-	m_pBloodSwitchLabel->SetPaintBackgroundEnabled(false);
-	m_pBloodSwitchLabel->SetBounds(340, 90, 100, 20);
-
-	m_pBloodSwitch = new ComboBox(this, "BloodSwitch", 2, false);
-	m_pBloodSwitch->AddItem(wstrSwitch[0], NULL);
-	m_pBloodSwitch->AddItem(wstrSwitch[1], NULL);
-	m_pBloodSwitch->ActivateItem(gConfigs.bEnableBlood ? 0 : 1);
-	m_pBloodSwitch->SetBounds(340, 120, 100, 24);
-
-	m_pLanguageSelectionLabel = new Label(this, "LanguageSelectionLabel", "#CSBTE_LanguageLabel");
-	m_pLanguageSelectionLabel->SetPaintBackgroundEnabled(false);
-	m_pLanguageSelectionLabel->SetBounds(10, 150, 100, 24);
-
-	m_pLanguageSelection = new ComboBox(this, "LanguageSelection", 3, false);
-	if (Q_stricmp(gConfigs.szLanguage, "tchinese"))
-	{
-		m_pLanguageSelection->AddItem(L"简体中文", NULL);
-		m_pLanguageSelection->AddItem(L"繁体中文", NULL);
-	}
-	else
-	{
-		m_pLanguageSelection->AddItem(L"簡體中文", NULL);
-		m_pLanguageSelection->AddItem(L"繁體中文", NULL);
-	}
-
-	m_pLanguageSelection->AddItem(L"English", NULL);
-	m_pLanguageSelection->SetBounds(120, 150, 100, 24);
-
-	if (!Q_stricmp(gConfigs.szLanguage, "schinese"))
-		m_pLanguageSelection->ActivateItem(0);
-	else if (!Q_stricmp(gConfigs.szLanguage, "tchinese"))
-		m_pLanguageSelection->ActivateItem(1);
-	else if (!Q_stricmp(gConfigs.szLanguage, "english"))
-		m_pLanguageSelection->ActivateItem(2);
-	else
-	{
-		m_pLanguageSelection->SetNumberOfEditLines(4);
-		m_pLanguageSelection->AddItem(gConfigs.szLanguage, NULL);
-		m_pLanguageSelection->ActivateItem(3);
-	}
-
-	m_pOKButton = new Button(this, "OKButton", "#GameUI_OK");
-	m_pOKButton->SetBounds(230, 150, 100, 24);
-	m_pOKButton->SetCommand("SaveConfigs");
-
-	m_pCancelButton = new Button(this, "CancelButton", "#GameUI_Cancel");
-	m_pCancelButton->SetBounds(340, 150, 100, 24);
+	m_pCancelButton = new Button(this, "CancelButton", "#GameUI_Close");
+	m_pCancelButton->SetContentAlignment(Label::a_center);
+	m_pCancelButton->SetBounds(260, 625, 200, 50);
 	m_pCancelButton->SetCommand("vguicancel");
-}
+	m_pCancelButton->SetVisible(true);
 
+	m_pUpdateNews = new RichText(this, "UpdateNews");
+	m_pUpdateNews->SetPaintBackgroundEnabled(false);
+	m_pUpdateNews->SetBounds(25, 30, 700, 600);
+	m_pUpdateNews->SetText("#CSBTE_UpdateNews_Data");
+}
 
 void CCSBTEUpdateDialog::OnCommand(const char *command)
 {
-	if (!Q_stricmp(command, "SaveConfigs"))
+	if (!Q_stricmp(command, "vguicancel"))
 	{
-		if (!m_pNewHUDSwitch->GetActiveItem())
-		{
-			if (!gConfigs.bEnableNewHud)
-				pfnUnknown();
-			gConfigs.bEnableNewHud = 1;
-		}
-		else
-		{
-			if (gConfigs.bEnableNewHud)
-			{
-				line *pLine = HudSayText().head;
-				while (pLine)
-				{
-					HudSayText().head = pLine;
-					pLine = pLine->next;
-					free(HudSayText().head);
-					HudSayText().head = NULL;
-				}
-			}
-			gConfigs.bEnableNewHud = 0;
-		}
-
-		if (!m_pNewMuzzleflashSwitch->GetActiveItem())
-			gConfigs.bEnableNewMuzzle = 1;
-		else
-			gConfigs.bEnableNewMuzzle = 0;
-
-		if (!m_pFireDlightSwitch->GetActiveItem())
-			gConfigs.bEnableFireDlight = 1;
-		else
-			gConfigs.bEnableFireDlight = 0;
-
-		if (!m_pBloodSwitch->GetActiveItem())
-			gConfigs.bEnableBlood = 1;
-		else
-			gConfigs.bEnableBlood = 0;
-
-		bool bClose = true;
-
-		if (m_pLanguageSelection->GetActiveItem() != 3)
-		{
-			if (
-				(m_pLanguageSelection->GetActiveItem() == 0 && Q_stricmp(gConfigs.szLanguage, "schinese")) ||
-				(m_pLanguageSelection->GetActiveItem() == 1 && Q_stricmp(gConfigs.szLanguage, "tchinese")) ||
-				(m_pLanguageSelection->GetActiveItem() == 2 && Q_stricmp(gConfigs.szLanguage, "english"))
-				)
-			{
-				bClose = false;
-
-				CCSBTEUpdateDialogMessageBox *msgbox;
-				msgbox = new CCSBTEUpdateDialogMessageBox(L"Warning", L"The language is only changed when you next run the game.\n* Your Steam Language, the game launcher and weapon editor's language will also be changed here\n* The Localized String Table File for languages except Chinese Simplified may not be full.", this);
-				msgbox->SetOKButtonText(L"Yes");
-				msgbox->SetCancelButtonText(L"Cancel");
-				msgbox->SetOKButtonVisible(true);
-				msgbox->SetCancelButtonVisible(true);
-				msgbox->SetBounds(GetWide() / 2 - 150, GetTall() / 2 - 100, 300, 200);
-
-				msgbox->Activate();
-
-				SetKeyBoardInputEnabled(false);
-				SetMouseInputEnabled(false);
-			}
-		}
-
-		Config_Save();
-		if (bClose)
-			Close();
-	}
-	else if (!Q_stricmp(command, "vguicancel"))
 		Close();
-	else if (!Q_stricmp(command, "OnOK"))
-	{
-		switch (m_pLanguageSelection->GetActiveItem())
-		{
-		case 0:
-			//Q_strcpy(gConfigs.szLanguage, "schinese");
-			system()->SetRegistryString("HKEY_CURRENT_USER\\Software\\Valve\\Steam\\Language", "schinese");
-			break;
-		case 1:
-			//Q_strcpy(gConfigs.szLanguage, "tchinese");
-			system()->SetRegistryString("HKEY_CURRENT_USER\\Software\\Valve\\Steam\\Language", "tchinese");
-			break;
-		case 2:
-			//Q_strcpy(gConfigs.szLanguage, "english");
-			system()->SetRegistryString("HKEY_CURRENT_USER\\Software\\Valve\\Steam\\Language", "english");
-			break;
-		}
-		SetKeyBoardInputEnabled(true);
-		SetMouseInputEnabled(true);
-		Close();
-
-		// Force restart of entire engine
-		engine->pfnClientCmd("fmod stop\n");
-		engine->pfnClientCmd("_restart\n");
 	}
-	else if (!Q_stricmp(command, "OnCancel"))
-	{
-		SetKeyBoardInputEnabled(true);
-		SetMouseInputEnabled(true);
-	}
-	else
-		BaseClass::OnCommand(command);
 }
---
+++ GameUI/CSBTEWpnDataEditor.cpp
@@ -0,0 +1,104 @@
+#include "base.h"
+#include "CSBTEWpnDataEditor.h"
+#include <vgui_controls\PHandle.h>
+#include "common.h"
+#include <io.h>
+#include <string>
+#include <fstream>
+
+//char WEAPON_TYPE[8][32] = { "#CSO_Whole", "#CSO_Inventory_Pistol", "#CSO_Inventory_ShotGun", "#CSO_Inventory_SubMachineGun", "#CSO_Inventory_Rifle", "#CSO_Inventory_MachineGun", "#CSO_Inventory_Knife", "#CSO_Inventory_Equipment" };
+extern wchar_t *GetWeaponNameFormat(char *name);
+CCSBTEWpnDataEditor::CCSBTEWpnDataEditor(Panel *parent, const char *panelName, bool showTaskbarIcon) : Frame(parent, panelName, showTaskbarIcon)
+{
+	int sw, sh;
+	surface()->GetScreenSize(sw, sh);
+
+	SetTitle("#CSBTE_WpnDataEditor_Title", false);
+
+	SetSize(740, 700);
+	MoveToCenterOfScreen();
+	SetSizeable(false);
+	SetVisible(true);
+
+	m_pCancelButton = new Button(this, "CancelButton", "#GameUI_Close");
+	m_pCancelButton->SetContentAlignment(Label::a_center);
+	m_pCancelButton->SetBounds(260, 625, 200, 50);
+	m_pCancelButton->SetCommand("vguicancel");
+	m_pCancelButton->SetVisible(true);
+
+	m_pNextWpn = new Button(this, "NextWeapon", "#CSBTE_WpnDataEditor_Next");
+	m_pNextWpn->SetContentAlignment(Label::a_center);
+	m_pNextWpn->SetBounds(500, 625, 200, 50);
+	m_pNextWpn->SetCommand("nextwpn");
+	m_pNextWpn->SetVisible(true);
+
+	for (auto &kv : CIniParser("weapons.ini"))
+	{
+		auto &wpnName = kv.first;
+
+		m_pName->SetText(GetWeaponNameFormat(const_cast<char *>(wpnName.c_str())));
+		m_pName->SetPaintBackgroundEnabled(false);
+		m_pName->SetBounds(25, 30, 100, 20);
+	}
+
+	//LoadWeaponsData();
+}
+
+void CCSBTEWpnDataEditor::LoadWeaponsData()
+{
+	//NextWpn();
+	for (auto &kv : CIniParser("weapons.ini"))
+	{
+		auto &wpnName = kv.first;
+
+		m_pName->SetText(GetWeaponNameFormat(const_cast<char *>(wpnName.c_str())));
+		m_pName->SetBounds(25, 30, 100, 20);
+	}
+}
+
+void CCSBTEWpnDataEditor::NextWpn()
+{
+	//Read Next Weapons//
+	auto wpnini = CIniParser("weapons.ini");
+	auto iterator = wpnini.begin();
+	auto kv1 = iterator;
+	++iterator;
+
+	if (iterator == wpnini.end())
+	{
+		CCSBTEWpnDataEditorMessageBox *msgbox;
+		msgbox = new CCSBTEWpnDataEditorMessageBox(L"#CSBTE_WpnDataEditor_Warning", L"#CSBTE_WpnDataEditor_WarningMsg", this);
+		msgbox->SetOKButtonText(L"Yes");
+		msgbox->SetCancelButtonText(L"Cancel");
+		msgbox->SetOKButtonVisible(true);
+		msgbox->SetCancelButtonVisible(true);
+		msgbox->SetBounds(GetWide() / 2 - 150, GetTall() / 2 - 100, 300, 200);
+		msgbox->Activate();
+		--iterator;
+	}
+
+	//Read Weapons Data//
+	for (auto &kv : CIniParser("weapons.ini"))
+	{
+		auto &wpnName = kv.first;
+		auto &wpnData = kv.second;
+
+		m_pName->SetText(GetWeaponNameFormat(const_cast<char *>(wpnName.c_str())));
+		m_pName->SetBounds(25, 60, 100, 20);
+
+		//m_pDamage = new Label(this, "WeaponsDamage", wpnData.c_str());
+	}
+	
+}
+
+void CCSBTEWpnDataEditor::OnCommand(const char *command)
+{
+	if (!Q_stricmp(command, "vguicancel"))
+	{
+		Close();
+	}
+	else if (!Q_stricmp(command, "nextwpn"))
+	{
+		NextWpn();
+	}
+}
