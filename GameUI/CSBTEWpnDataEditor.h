e824c83f561cf25aab3e41639c3ac6cf3b1b1f1d
HowToDoThis <nopeople012345@gmail.com>
2018-03-15 21:25:17 +0800

    Updated WpnDataEditor. Add Current WpnID and Total WeaponID.

--- GameUI/CSBTEWpnDataEditor.h
+++ GameUI/CSBTEWpnDataEditor.h
@@ -8,7 +8,12 @@
 #include <vgui_controls/ScrollBar.h>
 #include <vgui_controls/MessageBox.h>
 
+#include "PanelListPanel.h"
+#include "ScriptObject.h"
+
 #include <vector>
+#include <forward_list>
+#include <map>
 #include <util/iniparser.h>
 
 using namespace vgui;
@@ -38,15 +43,61 @@ public:
 	void SaveData();
 	void NextWpn();
 	void PrevWpn();
+	void CreateControls();
+	void DestroyControls();
+	void CountWpn();
 
 protected:
-	void OnCommand(const char *command);
+	virtual void OnCommand(const char *command) override;
+	virtual void OnClose() override;
 
 private:
-	CIniParser m_iniData;
-	CIniParser::iterator m_iniDataIterator;
-	Button * m_pCancelButton, *m_pNextWpn, *m_pPrevWpn, *bSaveData;
-	TextEntry *m_pDamage, *m_pDamageZombie, *m_pAttackInterval, *m_pMaxClip, *m_pMaxAmmo, *m_pMaxSpeed, *m_pReloadTime, *m_pDeployTime, *m_pKnockback, *m_pVelocityModifier, *m_pCost;
-	Label *m_pName, *m_plDamage, *m_plDamageZombie, *m_plAttackInterval, *m_plMaxClip, *m_plMaxAmmo, *m_plMaxSpeed, *m_plReloadTime, *m_plDeployTime, *m_plKnockback, *m_plVelocityModifier, *m_plCost;
-	CCSBTEWpnDataEditorMessageBox *msgbox;
+	struct WeaponInfoKey_Less
+	{
+		bool operator()(const std::string &lhs, const std::string &rhs) const;
+	};
+
+	using MyIni = CBasicIniParser<std::unordered_map<std::string, std::map<std::string, std::string, WeaponInfoKey_Less>>>;
+	MyIni m_iniData;
+	MyIni::iterator m_iniDataIterator;
+
+	Button *m_pNextWpn, *m_pPrevWpn, *m_pSaveButton;
+	//TextEntry *m_pDamage, *m_pDamageZombie, *m_pAttackInterval, *m_pMaxClip, *m_pMaxAmmo, *m_pMaxSpeed, *m_pReloadTime, *m_pDeployTime, *m_pKnockback, *m_pVelocityModifier, *m_pCost;
+	Label *m_pName, *curWpnID, *totalWpnID, *Divider;
+	CPanelListPanel *m_pListPanel;
+
+	class ControlPair : public vgui::Panel
+	{
+	public:
+		ControlPair(vgui::Panel *parent, char const *panelName) : Panel(parent, panelName), type(O_BADTYPE), pControl(NULL), pPrompt(NULL) {}
+
+	public:
+		virtual void OnSizeChanged(int wide, int tall)
+		{
+			int inset = 4;
+
+			if (pPrompt)
+			{
+				int w = wide / 2;
+
+				if (pControl)
+					pControl->SetBounds(w + 20, inset, w - 20, tall - 2 * inset);
+
+				pPrompt->SetBounds(0, inset, w + 20, tall - 2 * inset);
+			}
+			else
+			{
+				if (pControl)
+					pControl->SetBounds(0, inset, wide, tall - 2 * inset);
+			}
+		}
+		virtual ~ControlPair() {}
+
+	public:
+		objtype_t type;
+		std::string key;
+		vgui::Panel *pControl;
+		vgui::Label *pPrompt;
+	};
+
 };
